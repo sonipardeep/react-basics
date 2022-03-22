e1535b203c3927741e614e8b0e7c334ef19fc092
Pardeep kumar <kkmehran@gmail.com>
2022-03-22 18:15:31 +0500

    internal component state

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ objectID: 0,
 {
 title: 'Redux',
 url: 'https://github.com/reactjs/redux',
-author: 'Dan Abramov, Andrew Clark',
+author: 'Dan Abramov, Andrew Clar3k',
 num_comments: 2,
 points: 5,
 objectID: 1,
@@ -21,12 +21,20 @@ objectID: 1,
 ];
 
 class App extends Component {
+
+	constructor(props) {
+	super(props);
+	this.state = {
+	list: list,
+	};
+	}
+
 render() {
 
 return (
 
 	<div className="App">
-{list.map(item=> 
+{this.state.list.map(item=> 
 <div key={item.objectID}>
 <span>
 <a href={item.url}>{item.title}</a>
