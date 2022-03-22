b3a7bcb4ec990048ad8a3914dca149c921789ff0
Pardeep kumar <kkmehran@gmail.com>
2022-03-22 16:56:32 +0500

    react simple loop with arrow and curly braces and return removed function 5

--- src/App.js
+++ src/App.js
@@ -26,8 +26,7 @@ render() {
 return (
 
 	<div className="App">
-{list.map(item=> {
-return (
+{list.map(item=> 
 <div key={item.objectID}>
 <span>
 <a href={item.url}>{item.title}</a>
@@ -36,8 +35,7 @@ return (
 <span>{item.num_comments}</span>
 <span>{item.points}</span>
 </div>
-);
-})}
+)}
 	</div>
 
 );
