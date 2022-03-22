969c288b1d217e98327e10e26f917ad460f5e116
Pardeep kumar <kkmehran@gmail.com>
2022-03-22 16:48:39 +0500

    react simple loop 3

--- src/App.js
+++ src/App.js
@@ -26,18 +26,18 @@ render() {
 return (
 
 	<div className="App">
-	{list.map(function(item) {
-	return (
-	<div>
-	<span>
-	<a href={item.url}>{item.title}</a>
-	</span>
-	<span>{item.author}</span>
-	<span>{item.num_comments}</span>
-	<span>{item.points}</span>
-	</div>
-	);
-	})}
+{list.map(function(item) {
+return (
+<div key={item.objectID}>
+<span>
+<a href={item.url}>{item.title}</a>
+</span>
+<span>{item.author}</span>
+<span>{item.num_comments}</span>
+<span>{item.points}</span>
+</div>
+);
+})}
 	</div>
 
 );
