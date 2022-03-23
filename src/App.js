5a6483cabc8295872bd6234be82570db608335f8
Pardeep kumar <kkmehran@gmail.com>
2022-03-23 18:39:33 +0500

    Interactions with forms and events

--- src/App.js
+++ src/App.js
@@ -20,21 +20,36 @@ const list = [
 },
 ];
 
+function isSearched(searchTerm) {
+	return function(item) {
+		return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+	}
+}
+
+
 class App extends Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
 			list,
+			searchTerm: '',
 		};
+
+		this.onSearchChange = this.onSearchChange.bind(this);
 
 		this.onDismiss = this.onDismiss.bind(this);
 	}
 
 	onDismiss(id) {
-	const isNotId = item => item.objectID !== id;
-	const updatedList = this.state.list.filter(isNotId);
-	this.setState({ list: updatedList });
+		const isNotId = item => item.objectID !== id;
+		const updatedList = this.state.list.filter(isNotId);
+		this.setState({ list: updatedList });
+	}
+
+
+	onSearchChange(event) {
+		this.setState({ searchTerm: event.target.value });
 	}
 
 /*
@@ -69,32 +84,35 @@ const updatedList = this.state.list.filter(item => item.objectID !== id);
 
 
 
-	render() {
+render() {
 
-		return (
+	return (
 
-			<div className="App">
-			{this.state.list.map(item=> 
-				<div key={item.objectID}>
-				<span>
-				<a href={item.url}>{item.title}</a>
-				</span>
-				<span>{item.author}</span>
-				<span>{item.num_comments}</span>
-				<span>{item.points}</span>
-				<span>
-				<button
-				onClick={() => this.onDismiss(item.objectID)}
-				type="button"
-				>
-				Dismiss
-				</button>
-				</span>
-				</div>
-				)}
+		<div className="App">
+		<form>
+		<input type="text" onChange={this.onSearchChange} />
+		</form>
+		{this.state.list.filter(isSearched(this.state.searchTerm)).map(item=> 
+			<div key={item.objectID}>
+			<span>
+			<a href={item.url}>{item.title}</a>
+			</span>
+			<span>{item.author}</span>
+			<span>{item.num_comments}</span>
+			<span>{item.points}</span>
+			<span>
+			<button
+			onClick={() => this.onDismiss(item.objectID)}
+			type="button"
+			>
+			Dismiss
+			</button>
+			</span>
+			</div>
+			)}
 			</div>
 
 			);
-		}
-	}
-	export default App;
+}
+}
+export default App;
