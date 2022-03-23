64e9c257b435a366a4bb08f77a495f4d9a447d4d
Pardeep kumar <kkmehran@gmail.com>
2022-03-23 18:46:55 +0500

    ES6 Destructing

--- src/App.js
+++ src/App.js
@@ -85,14 +85,14 @@ const updatedList = this.state.list.filter(item => item.objectID !== id);
 
 
 render() {
-
+	const { searchTerm, list } = this.state;
 	return (
 
 		<div className="App">
 		<form>
 		<input type="text" onChange={this.onSearchChange} />
 		</form>
-		{this.state.list.filter(isSearched(this.state.searchTerm)).map(item=> 
+		{list.filter(isSearched(searchTerm)).map(item=> 
 			<div key={item.objectID}>
 			<span>
 			<a href={item.url}>{item.title}</a>
